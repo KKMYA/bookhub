84a00cc9492f022d8727ea17be3a6a4fd6f3b8c2
alexandrernd <alexandre.renard+1@gmail.com>
2026-04-22 10:09:28 +0200

    Add register page

--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { Input } from "../../ui/components/input/input";
+import { PasswordInput } from "../../ui/components/input-password/input-password";
+import { Button } from "../../ui/components/button/button";
 
 @Component({
-  selector: 'app-register',
-  imports: [],
-  templateUrl: './register.html',
-  styleUrl: './register.css',
+    selector: 'app-register',
+    imports: [Input, PasswordInput, Button],
+    templateUrl: './register.html'
 })
-export class Register {}
+
+export class Register {
+
+}
