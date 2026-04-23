dd7f5990172f010d721e0381a55e0df7ae84a890
agnes-guillet21 <agnes.guillet@hotmail.fr>
2026-04-23 20:49:04 +0200

    Feat:[BOOK] maj dto

--- backend/src/main/java/com/eni/bookhub/controller/dto/response/BookSumaryDto.java
+++ backend/src/main/java/com/eni/bookhub/controller/dto/response/BookSumaryDto.java
@@ -7,11 +7,15 @@ import lombok.Builder;
 @Schema(description = "Data Transfer Object representing book for the homepage.")
 public record BookSumaryDto(
 
+        Integer idBook,
+
         String titre,
 
         String auteur,
 
         Double noteMoyenne,
+
+        Integer nbExemplairesDisponibles,
 
         String couvertureUrl,
 
