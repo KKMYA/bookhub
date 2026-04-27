a5a2ea8bfba310546b3e739f46c4dc9e156a9b68
agnes-guillet21 <agnes.guillet@hotmail.fr>
2026-04-27 14:41:13 +0200

    Feat: [CATEGORY]: create controller mapper , dto, service for category

---
+++ backend/src/main/java/com/eni/bookhub/controller/dto/response/CategoryDto.java
@@ -0,0 +1,4 @@
+package com.eni.bookhub.controller.dto.response;
+
+public record CategoryDto() {
+}
--- backend/src/main/java/com/eni/bookhub/repository/CategoryRepository.java
+++ backend/src/main/java/com/eni/bookhub/repository/CategoryRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
     Optional<Category> findByLibelle(String name);
 
 }
